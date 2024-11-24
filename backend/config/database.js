f903df211d3d3399a42dc547134f62bcc7ecb13e
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-11-24 14:00:01 -0300

    fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,12 +12,12 @@ const sequelize = new Sequelize(
   }
 );
 
-//sequelize.sync({ force: true})
-//.then(() => {
-//console.log('Database synced successfully');
-//})
-//.catch((error) => {
-//console.error('Error syncing database', error);
-//});
+sequelize.sync({ force: true})
+.then(() => {
+console.log('Database synced successfully');
+})
+.catch((error) => {
+console.error('Error syncing database', error);
+});
 
 module.exports = sequelize;
