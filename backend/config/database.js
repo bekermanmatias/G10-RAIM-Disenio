b3c9a6fd759777c7602b98441adb7d54205cd349
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-11-24 13:42:13 -0300

    back subido render

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,16 @@
+require('dotenv').config(); // Carga las variables de entorno desde .env
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('practicayo', 'root', '123456', {
- host: 'localhost',
- dialect: 'mysql'
-});
+// Configuración de Sequelize usando variables de entorno
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     // Nombre de la base de datos
+  process.env.DB_USER,     // Usuario
+  process.env.DB_PASSWORD, // Contraseña
+  {
+    host: process.env.DB_HOST,   // Host de la base de datos
+    dialect: process.env.DB_DIALECT // Dialecto (MySQL, Postgres, etc.)
+  }
+);
 
 //sequelize.sync({ force: true})
 //.then(() => {
