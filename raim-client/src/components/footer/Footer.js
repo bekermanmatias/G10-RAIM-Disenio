4eec2c52c26191e8076eccab26c87a01f4d9b963
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-11-24 21:05:56 -0300

    add: footer

---
+++ raim-client/src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+// src/components/footer/Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>© 2024 RAIM - Grupo 10 - Diseño de Sistemas - UTN FRLP</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
