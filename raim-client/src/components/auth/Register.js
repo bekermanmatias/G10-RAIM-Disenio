4e9d288e6585e8eaa722cecf74ea7c7613fcdbe0
Matias Rau Bekerman <bekermanmatias@gmail.com>
2024-11-27 12:47:41 -0300

    fix: register front

--- raim-client/src/components/auth/Register.js
+++ raim-client/src/components/auth/Register.js
@@ -8,7 +8,9 @@ import {
     Heading,
     Container,
     Flex,
+    IconButton,
 } from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'; // Añadir esta importación
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../utils/api'; 
 import CustomButton from '../../utils/CustomButton';
@@ -23,7 +25,8 @@ const Register = () => {
         department: ''
     });
 
-    const [departments, setDepartments] = useState([]); // Estado para almacenar los departamentos
+    const [showPassword, setShowPassword] = useState(false); // Añadir estado para mostrar/ocultar contraseña
+    const [departments, setDepartments] = useState([]); 
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -32,7 +35,7 @@ const Register = () => {
                 const response = await fetch('https://g10-raim-disenio.onrender.com/api/departamento');
                 if (response.ok) {
                     const data = await response.json();
-                    setDepartments(data); // Asumiendo que la respuesta es un array de departamentos
+                    setDepartments(data);
                 } else {
                     console.error('Error al obtener los departamentos:', response.statusText);
                 }
@@ -42,7 +45,7 @@ const Register = () => {
         };
 
         fetchDepartments();
-    }, []); // Se ejecuta una vez al montar el componente
+    }, []); 
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -55,10 +58,10 @@ const Register = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await api.register(formData); // Asegúrate de que la función register esté definida en tu API
+            const response = await api.register(formData);
             if (response.status === 200) {
                 console.log('Registro exitoso');
-                navigate('/login'); // Redirigir al inicio de sesión después del registro
+                navigate('/login');
             }
         } catch (error) {
             console.error('Error al registrar:', error);
@@ -66,101 +69,115 @@ const Register = () => {
     };
 
     const handleCancel = () => {
-        navigate('/login'); // Redirigir al inicio de sesión al cancelar
+        navigate('/login');
     };
 
     return (
-        <Container maxW="container.md" mt={10}>
-            <Box p={6} borderWidth={1} borderRadius="md" boxShadow="lg">
-                <Heading as="h2" size="lg" mb={4}>
-                    Registro
-                </Heading>
-                <form onSubmit={handleSubmit}>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Nombre Completo</FormLabel>
-                        <Input
-                            type="text"
-                            name="fullName"
-                            value={formData.fullName}
-                            onChange={handleChange}
-                            placeholder="Ingresa tu nombre completo"
-                        />
-                    </FormControl>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Correo Electrónico</FormLabel>
-                        <Input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            placeholder="Ingresa tu correo electrónico"
-                        />
-                    </FormControl>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Nombre de Usuario</FormLabel>
-                        <Input
-                            type="text"
-                            name="username"
-                            value={formData.username}
-                            onChange={handleChange}
-                            placeholder="Ingresa tu nombre de usuario"
-                        />
-                    </FormControl>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Contraseña</FormLabel>
-                        <Input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            placeholder="Ingresa tu contraseña"
-                        />
-                    </FormControl>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Cargo</FormLabel>
-                        <Input
-                            type="text"
-                            name="cargo"
-                            value={formData.cargo}
-                            onChange={handleChange}
-                            placeholder="Ingresa tu cargo"
-                        />
-                    </FormControl>
-                    <FormControl mb={4} isRequired>
-                        <FormLabel>Departamento</FormLabel>
-                        <Select
-                            name="department"
-                            value={formData.department}
-                            onChange={handleChange}
-                            placeholder="Selecciona un departamento"
-                        >
-                            {departments.map((department) => (
-                                <option key={department.idDepartamento} value={ department.nombre}>
-                                    {department.nombre}
-                                </option>
-                            ))}
-                        </Select>
-                    </FormControl>
-                    <Flex justifyContent="flex-end" mt={4}>
-                        <CustomButton 
-                            variant="cancel" 
-                            onClick={handleCancel} 
-                            width="100px"
-                            mr={2}
-                        >
-                            Cancelar
-                        </CustomButton>
-                        <CustomButton 
-                            variant="apply" 
-                            type="submit" 
-                            width="100px"
-                        >
-                            Registrarse
-                        </CustomButton>
-                    </Flex>
-                </form>
-            </Box>
-        </Container>
+        <Box display="flex" flexDirection="column" minHeight="84vh" bg="gray.100">
+            <Container maxW="container.md" mt={10}>
+                <Box p={6} bg="white" borderWidth={1} borderRadius="md" boxShadow="lg">
+                    <Heading as="h2" size="lg" mb={4}>
+                        Registro
+                    </Heading>
+                    <form onSubmit={handleSubmit}>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Nombre Completo</FormLabel>
+                            <Input
+                                type="text"
+                                name="fullName"
+                                value={formData.fullName}
+                                onChange={handleChange}
+                                placeholder="Ingresa tu nombre completo"
+                            />
+                        </FormControl>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Correo Electrónico</FormLabel>
+                            <Input
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                placeholder="Ingresa tu correo electrónico"
+                            />
+                        </FormControl>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Nombre de Usuario</FormLabel>
+                            <Input
+                                type="text"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                placeholder="Ingresa tu nombre de usuario"
+                            />
+                        </FormControl>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Contraseña</FormLabel>
+                            <Box position="relative">
+                                <Input
+                                    type={showPassword ? 'text' : 'password'}
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    placeholder="Ingresa tu contraseña"
+                                />
+                                <IconButton
+                                    position="absolute"
+                                    right="0"
+                                    top="50%"
+                                    transform="translateY(-50%)"
+                                    variant="link"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                    icon={showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                                    aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
+                                />
+                            </Box>
+                        </FormControl>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Cargo</FormLabel>
+                            <Input
+                                type=" text"
+                                name="cargo"
+                                value={formData.cargo}
+                                onChange={handleChange}
+                                placeholder="Ingresa tu cargo"
+                            />
+                        </FormControl>
+                        <FormControl mb={4} isRequired>
+                            <FormLabel>Departamento</FormLabel>
+                            <Select
+                                name="department"
+                                value={formData.department}
+                                onChange={handleChange}
+                                placeholder="Selecciona un departamento"
+                            >
+                                {departments.map((department) => (
+                                    <option key={department.idDepartamento} value={department.nombre}>
+                                        {department.nombre}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <Flex justifyContent="flex-end" mt={4}>
+                            <CustomButton 
+                                variant="cancel" 
+                                onClick={handleCancel} 
+                                width="100px"
+                                mr={2}
+                            >
+                                Cancelar
+                            </CustomButton>
+                            <CustomButton 
+                                variant="apply" 
+                                type="submit" 
+                                width="100px"
+                            >
+                                Registrarse
+                            </CustomButton>
+                        </Flex>
+                    </form>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
